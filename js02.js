90293c6763239731dc0e5ec02fb15977a04cf7d3
김태동 <yenafirst91@gmail.com>
2016-08-29 00:14:58 +0900

    javascript - Array

---
+++ js02.js
@@ -0,0 +1,25 @@
+/*
+배열 (Array)
+*/
+
+var array = [];
+var array2 = [1, "Hello", [1,2,3], { hi : 1}];
+
+var aney = ['Aney', 'Kim'];
+
+aney[0]; //'Aney'
+aney[1]; //'Kim'
+
+var array = ['hi', 'js', 123];
+
+var object = {
+	0: 'hi',
+	1: 'js',
+	2: 123
+};
+
+var array = new Array();
+array[0] = 1;
+array[1] = 'Hello';
+
+//배열 리터럴 : new를 사용하지 않고 [ ]만 사용해서 만든 배열을 배열 리터럴
